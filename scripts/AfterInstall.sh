2c4425fab545e04aaf9949e15de8ceb21f3ed71b
Paul Carnibella <paulcarnibella@gmail.com>
2017-03-05 16:48:13 +1100

    Fucking with the deploy

--- scripts/AfterInstall.sh
+++ scripts/AfterInstall.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-# cd /app/psc-bookings/
-# RAILS_ENV=production bundle install --path vendor/bundle
-# RAILS_ENV=production bundle exec rake db:migrate
-# RAILS_ENV=production bundle exec rake assets:clobber
-# RAILS_ENV=production bundle exec rake assets:precompile
+su deploy && cd ~
+source ~/environments
+cd /app/psc-bookings/
+~/.rbenv/shims/bundle exec rake db:migrate
