6e2d1677fd482e1147f882e3a7ae468c31c016c4
Paul Carnibella <paulcarnibella@gmail.com>
2017-03-13 10:28:26 +1100

    Asset precompilation?

--- scripts/AfterInstall.sh
+++ scripts/AfterInstall.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 sudo chown -R deploy:deploy /app/psc-bookings
-source ~/.bash_profile
-source ~/environments
-cd /app/psc-bookings/
-bundle install
-bundle exec rake db:migrate
 ~/stop-psc-bookings && ~/start-psc-bookings
